bb63e67aa01eca294897bc0cd5821425375f26eb
xzxz <2848159093@qq.com>
2026-01-21 22:54:53 +0800

    指针1_21

--- 鹏哥c语言笔记/06.指针/task.c
+++ 鹏哥c语言笔记/06.指针/task.c
@@ -1,20 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-//6.1.1指针类型
-int main()
-{
-	int a = 0x11223344;
-	char* pc = (char*)&a;
-	//int* pc = &a;
-	
-	*pc = 0;
-	printf("%x\n", a);
-	//只修改了一个字节
-	//指针类型决定了指针在被解引用时访问几个字节
-
-	return 0;
-}
+////6.1.1指针类型
+//int main()
+//{
+//	int a = 0x11223344;
+//	char* pc = (char*)&a;
+//	//int* pc = &a;
+//	
+//	*pc = 0;
+//	printf("%x\n", a);
+//	//只修改了一个字节
+//	//指针类型决定了指针在被解引用时访问几个字节
+//
+//	return 0;
+//}
 
 ////6.1.2
 //int main()
@@ -106,3 +106,22 @@ int main()
 //
 //	return 0;
 //}
+
+//一些题目
+//小端序：LSB在最低位 arr[0]:0x00000001 存储：01 00 00 00
+int main()
+{
+	int arr[] = { 1,2,3,4,5 };
+	short* p = (short*)arr; //解引用和自增时访问两个字节
+	int i = 0;
+	for (i = 0; i < 4; i++)
+	{
+		*(p + i) = 0; //连续四次把两个字节赋值为0
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]); //0，0，3，4，5
+	}
+
+	return 0;
+}
